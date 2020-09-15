0c9db4fe8f37ffe9b107a0b862dd6bf8a418cd8e
Natalie Wee <natalieweesh@gmail.com>
2020-09-15 02:21:27 -0400

    only reshuffle all cards after the third round

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,6 +22,7 @@ const Game = ({ location }) => {
   const [finishedGame, setFinishedGame] = useState(false);
   const [poop, setPoop] = useState(false);
   const [cards, setCards] = useState(tiles["tiles"]);
+  const [allTiles, setAllTiles] = useState(tiles["tiles"]);
   const [piles, setPiles] = useState([]);
   const [hands, setHands] = useState([]);
   const draggables = useRef([]);
@@ -40,28 +41,6 @@ const Game = ({ location }) => {
     } else {
       className = 'item inPile';
     }
-    // if (x > (360 - 100) && x < (1340 + 100) && y > (480-100) && y < (1160 + 100) && user) {
-    //   console.log("in no mans land", x, y, user)
-    //   className = 'item';
-    // } else if (user?.orderIndex === 0 && x < 1340 && x > 360 && y > 120 && y < (480-100)) {
-    //   console.log("in player 1 zone")
-    //   className = 'item player-0-private';
-    // } else if (user?.orderIndex === 1 && x > 0 && x < (360-100) && y > 480 && y < 1160) {
-    //   console.log("in player 2 zone")
-    //   className = 'item player-1-private';
-    // } else if (user?.orderIndex === 2 && x > (1340+100) && y > 480 && y < 1160) {
-    //   className = 'item player-2-private';
-    //   console.log('in player 3 zone')
-    // } else if (user?.orderIndex === 3 && x > 360 && x < 1340 && y > (1160-100)) {
-    //   className = 'item player-3-private';
-    //   console.log('in player 4 zone')
-    // } else {
-    //   if ((x >= 1225 && x <= 1304 && y >= 461 && y <= 1190) || (x >= 405 && x <= 479 && y >= 460 && y <= 1180)) {
-    //     className = 'item poop sideways';
-    //   } else {
-    //     className = 'item poop';
-    //   }      
-    // }
     
     if (animate) {
       let oldClassName = className;
@@ -101,40 +80,20 @@ const Game = ({ location }) => {
 
   useEffect(() => {
     socket.off('roomData').on('roomData', ({ users }) => {
-      // console.log('Room Data', users)
       setUsers(users);
       allUsers.current = users;
     })
     socket.off('gameStatus').on('gameStatus', ({ game }) => {
-      // console.log('Game Status', game);
-      if (game && game.finishedGame) {
-        // game.hands.map((hand, i) => {
-        //   hand.map((card) => {
-        //     socket.emit('moveTile', {el: card.id, x: card.x, y: (i * 360) + card.y}, () => {});
-        //   })
-        // })
-        // let allHandTiles = document.getElementsByClassName('inHand')
-        // for (let z = 0; z < allHandTiles.length; z++) {
-        //   allHandTiles[z].className = 'item';
-        // }
-      }
       if (game && game.cards.length > 0) {
         setCards(game.cards);
         setPiles(game.piles);
-        // setHands(game.hands);
         const user = allUsers.current.find((user) => user.name === myName.current);
-        // console.log('User orderindex', user.orderIndex)
-        if (!user.answerSubmitted) {
-          // console.log('not answer submitted!')
-        }
         if (game.hands) {
-        // if (game.hands[user.orderIndex] && game.hands[user.orderIndex].length <= game.currentRound) {
           // console.log('user pile', game.piles[(user.orderIndex + game.currentRound) % game.userCount])
           var allTiles = document.getElementsByClassName('item');
           for (let i=0; i < allTiles.length; i++) {
             allTiles[i].className = 'item hidden';
           }
-          // console.log('game pile index?', (user.orderIndex + game.currentRound) % game.userCount)
           game.piles[(user.orderIndex + game.currentRound) % game.userCount].map((c) => {
             if (c && document.getElementById(c['id'])) {
               if (user.answerSubmitted) {
@@ -147,50 +106,28 @@ const Game = ({ location }) => {
                 } else {
                   const randPileX = 370 + Math.floor(Math.random() * 350);
                   const randPileY = 130 + (user.orderIndex * 365) + Math.floor(Math.random() * 280);
-                  tile.set(randPileX, randPileY);
-                  console.log("move it randomly!", c['x'], c['y'], c, tile)
-                  socket.emit('moveTile', {el: c['id'], x: randPileX, y: randPileY, settingUp: true}, () => {
-                    console.log('DONE moving tile to random', randPileX, randPileY, c['id']);
-                  });
+                  if (tile) {
+                    tile.set(randPileX, randPileY);
+                    socket.emit('moveTile', {el: c['id'], x: randPileX, y: randPileY, settingUp: true}, () => {});
+                  }
                 }
               }
             }
           })
-          // console.log('USER hand', game.hands[user.orderIndex])
 
-          // game.hands[user.orderIndex].map((c) => {
-          //   console.log(document.getElementById(c['id']));
-          //   if (document.getElementById(c['id'])) {
-          //     document.getElementById(c['id']).className = 'item inHand';
-          //   }
-          //   // move to previous location if location is already set
-          //   if (c['x'] && c['y']) {
-          //     console.log("it has an x and y!",)
-          //     let tile = draggables.current.find((d) => d.element.id === c.id);
-          //     tile.set(c['x'], c['y']);
-          //   }
-          // })
           game.hands.map((hand, handIdx) => {
-            // console.log('hand', hand, 'handidx', handIdx)
-            // const user = allUsers.current.find((user) => user.name === myName.current);
-            // console.log("USER?", user)
             let cards;
             if (handIdx === user.orderIndex || game.finishedGame) {
-              // console.log('this hand is the users hand', handIdx, user.orderIndex, hand)
               cards = hand;
             } else {
               cards = hand.slice(0, game.currentRound);
-              // console.log('this hand is sliced', handIdx, user.orderIndex, cards)
             }
-            // console.log('cards for handidx', handIdx, cards)
             cards.map((c) => {
-              // console.log('C in hand', document.getElementById(c['id']));
               if (document.getElementById(c['id'])) {
                 document.getElementById(c['id']).className = 'item inHand';
               }
               // move to previous location if location is already set
               if (c['x'] && c['y']) {
-                // console.log("it has an x and y!",)
                 let tile = draggables.current.find((d) => d.element.id === c.id);
                 tile.set(c['x'], c['y']);
               }
@@ -209,6 +146,13 @@ const Game = ({ location }) => {
       }
     })
     socket.off('gameRestarted').on('gameRestarted', ({ users }) => {
+      draggables.current = [];
+      setCards(tiles["tiles"])
+      document.querySelectorAll('.item').forEach((el) => {
+        draggables.current.push(new Draggable(el, {onDragEnd: (el, x, y, event) => {
+          socket.emit('moveTile', {el: el.id, x, y, settingUp: false}, () => {})
+        }}));
+      })
       setFinishedGame(false)
       setNewRound(false)
       setUsers(users);
@@ -221,15 +165,14 @@ const Game = ({ location }) => {
       })
     })
     socket.off('tileMoved').on('tileMoved', ({el, x, y, user, settingUp}) => {
-      if (user.name !== myName.current || settingUp){
-        console.log('tile was moved by someone else or setting up!')
+      if (user.name !== myName.current || settingUp) {
+        // console.log('tile was moved by someone else or setting up!')
         return;
       }
       const tile = draggables.current.filter((e) => e.element.id === el)[0];
       if (document.getElementById(el) && (document.getElementById(el).className.includes('hidden') || document.getElementById(el).className.includes('inPile'))) {
         return
       }
-      console.log('received tilemoved', el, x, y, user);
       setTileClass(x, y, user, tile, true);
       tile.set(x, y);
     })
@@ -237,7 +180,7 @@ const Game = ({ location }) => {
     document.querySelectorAll('.item').forEach((el) => {
       draggables.current.push(new Draggable(el, {onDragEnd: (el, x, y, event) => {
         socket.emit('moveTile', {el: el.id, x, y, settingUp: false}, () => {
-          console.log('tile moved on Drag End!', el.id, x, y);
+          // console.log('tile moved on Drag End!', el.id, x, y);
         })
       }}));
     })
@@ -247,7 +190,6 @@ const Game = ({ location }) => {
       const randY = 120 + Math.floor(Math.random() * (1080));
       d.set(randX, randY);
     })
-    // console.log('dragagbles are now', draggables.current)
   }, [])
 
   useEffect(() => {
@@ -305,21 +247,10 @@ const Game = ({ location }) => {
   useEffect(() => {
     socket.off('startGame').on('startGame', ({ users }) => {
       socket.emit('initiateGame', {cards: tiles["tiles"]}, () => {
-        console.log("INITIATED GAME")
-        socket.emit('fetchGame', () => {
-        })
+        socket.emit('fetchGame', () => {})
       })
     })
   }, [currentGame, setCurrentGame])
-
-  const restartGame = (event) => {
-    event.preventDefault();
-    socket.emit('restartGame', () => {
-      socket.emit('fetchGame', () => {
-      })
-    })
-  }
-
 
   const submitTurn = () => {
     let cardsInHand = [];
@@ -327,7 +258,6 @@ const Game = ({ location }) => {
     let inHand = document.getElementsByClassName('inHand');
     for (let i=0; i < inHand.length; i++) {
       let c = inHand[i];
-      // console.log('submitting inhand', c.id, c.offsetLeft, c.offsetTop)
       if (c.offsetLeft >= 850 && c.offsetTop >= 120 + (user.orderIndex * 371) && c.offsetTop < 120 + ((user.orderIndex + 1) * 371)) {
         cardsInHand.push({id: c.id, img: c.dataset['img'], type: c.dataset['type'], x: c.offsetLeft, y: c.offsetTop});
         socket.emit('moveTile', {el: c.id, x: c.offsetLeft, y: c.offsetTop}, () => {});
@@ -338,7 +268,6 @@ const Game = ({ location }) => {
     let inPile = document.getElementsByClassName('inPile');
     for (let j=0; j < inPile.length; j++) {
       let c = inPile[j];
-      // console.log('submitting inpile', c.id, c.offsetLeft, c.offsetTop)
       if (c.offsetLeft < 850 && c.offsetTop >= 120 + (user.orderIndex * 371) && c.offsetTop < 120 + ((user.orderIndex + 1) * 371)) {
         cardsInPile.push({id: c.id, img: c.dataset['img'], type: c.dataset['type']});
       } else if (c.offsetLeft >= 850 && c.offsetTop >= 120 + (user.orderIndex * 371) && c.offsetTop < 120 + ((user.orderIndex + 1) * 371)) {
@@ -346,11 +275,9 @@ const Game = ({ location }) => {
         socket.emit('moveTile', {el: c.id, x: c.offsetLeft, y: c.offsetTop}, () => {});
       }
     }
-    console.log("SUbmitting TUrn", cardsInHand, cardsInPile)
     socket.emit('submitTurn', {cardsInHand: cardsInHand, cardsInPile: cardsInPile}, () => {
       const user = allUsers.current.find((user) => user.name === myName.current);
       if (user.answerSubmitted) {
-        // console.log('hide the tiles now!!!!')
         // now hide the pile since you submitted already
         let newHandTiles = document.getElementsByClassName('inHand')
         let allTiles = document.getElementsByClassName('item');
@@ -364,7 +291,6 @@ const Game = ({ location }) => {
         }
 
       }
-      // console.log('submitted turn', cardsInHand, cardsInPile);
     })
   }
 
@@ -378,15 +304,10 @@ const Game = ({ location }) => {
   const userRestart = (event) => {
     event.preventDefault();
 
-    socket.emit('setReadyToRestart', {cards: tiles["tiles"]}, () => {
-    })
+    socket.emit('setReadyToRestart', {cards: currentGame.gameNumber === 3 ? (tiles["tiles"]) : (currentGame.leftoverCards || tiles["tiles"]), newGameOf3: currentGame.gameNumber === 3}, () => {})
   }
 
-  // const user = users.find((user) => user.name === name);
-  const user = allUsers.current.find((user) => user.name === myName.current);
-
-  // console.log("USER", user);
-  // console.log('current game', currentGame)
+  const user = users.find((user) => user.name === myName.current);
   return (
     <div className={`player-${user?.orderIndex} outerContainer ${currentGame.finishedGame && 'revealAll'}`}>
       <div className="rightTopSquare instructions">
@@ -395,7 +316,6 @@ const Game = ({ location }) => {
           <p className="bold">🤗 Scoreboard! 🤗</p>
           {currentGame.scores.map((score, i) => {
             return <div><label>{users[i]['name']}: &nbsp;</label><input onChange={(e) => {
-              // console.log('user', i, e.target.value);
               socket.emit('updateScore', {userId: i, score: e.target.value}, () => {})
             }} type="number" value={score}/></div>
           })}
@@ -419,21 +339,14 @@ const Game = ({ location }) => {
       <div className={"sideContainer player-"+user?.orderIndex}>
         {poop ? <div className="modal"><div className="attentionModal">Hey! Pay attention to the game!<button className="button" onClick={() => {setPoop(false)}}>Ok</button></div></div> : null}
         {(currentGame.length === 0 || finishedGame) && <TextContainer users={users} user={user} game={currentGame} finishedGame={finishedGame} />}
-        {currentGame.length === 0 && users.length > 1 && <button className="startButton" disabled={user?.readyToPlay} onClick={updateUserStatus}>{user?.readyToPlay ? 'Waiting for other players' : 'Ready to play!'}</button>}
+        {currentGame.length === 0 && users.length > 1 && <button className="startButton" disabled={user?.readyToPlay || user?.readyToRestart} onClick={updateUserStatus}>{(user?.readyToPlay || user?.readyToRestart) ? 'Waiting for other players' : 'Ready to play!'}</button>}
         {finishedGame && <div><button className="startButton" disabled={user?.readyToRestart} onClick={userRestart}>{user?.readyToRestart ? 'Waiting for other players' : 'Play again!'}</button></div>}
 
         {currentGame.length !== 0 && (
           <>
+          {currentGame && <p>Game {currentGame.gameNumber} of 3</p>}
           {(user && !user.answerSubmitted) ? <button className="submitButton" onClick={submitTurn}>Finalize your turn!</button> : (currentGame?.finishedGame ? null : <p>Waiting for other players before flipping over your card...</p>)}
           {currentGame.finishedGame ? <p>Game over!</p> : <p>Round {currentGame.currentRound + 1} of {currentGame.handSize}</p>}
-          {/* {!finishedGame && <button className="endGame" onClick={() => {
-            const endIt = window.confirm('Are you sure you want to end the game?');
-            if (endIt) {
-              socket.emit('showAllTiles', () => {
-                console.log('now show all tiles')
-              })
-            }
-          }}>Click here to end the game and reveal all the tiles!</button>} */}
           </>
         )}
         <button className="rulesButton" onClick={() => setModal('rules')}>Check the rule book</button>
